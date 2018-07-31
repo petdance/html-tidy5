51189b49f01e111d2a9516e6100040ab96c12b94
Andy Lester <andy@petdance.com>
2018-07-31 15:03:15 -0500

    Add a test for show-info

--- t/TidyTestUtils.pm
+++ t/TidyTestUtils.pm
@@ -4,10 +4,13 @@ use 5.010001;
 use warnings;
 use strict;
 
+use Test::More;
+
 use base 'Exporter';
 
 our @EXPORT_OK = qw(
     remove_specificity
+    messages_are
 );
 our @EXPORT = @EXPORT_OK;
 
@@ -17,6 +20,16 @@ sub remove_specificity {
     $clean =~ s/HTML Tidy for HTML5 for \w+ version \d+\.\d+\.\d+/TIDY/;
 
     return $clean;
+}
+
+sub messages_are {
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+
+    my $tidy     = shift;
+    my $expected = shift;
+    my $msg      = shift;
+
+    return is_deeply( [ map { $_->as_string } $tidy->messages ], $expected, $msg );
 }
 
 1;
